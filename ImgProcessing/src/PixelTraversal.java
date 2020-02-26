50d92debb3112739f748cf18ed0ac2de6ad39d92
朱剑锋 <2445843238@qq.com>
2020-02-26 13:57:47 +0800

    Update PixelTraversal.java

--- ImgProcessing/src/PixelTraversal.java
+++ ImgProcessing/src/PixelTraversal.java
@@ -50,5 +50,44 @@ public class PixelTraversal {
             e1.printStackTrace();
         }
     }
+     // 添加一个参数，该参数表示处理图片的格式
+    public void transform(String format, String imagePath, String path) throws   Exception {
+        BufferedImage image;
+
+        int[] rgb = new int[3];
+        try {
+            image = ImageIO.read(new File(imagePath));
+
+            //输出图片大小
+            System.out.println(image.getHeight() + "    " + image.getWidth());
+            for (int y = image.getMinY(); y < image.getHeight(); y++) {
+                for (int x = image.getMinX(); x < image.getWidth(); x++) {
+                    int pixel = image.getRGB(x, y);
+
+                    rgb[0] = (pixel & 0xff0000) >> 16; //r
+                    rgb[1] = (pixel & 0xff00) >> 8; //g
+                    rgb[2] = (pixel & 0xff); //b
+                    // 如果像素值接近纯白色，直接将像素赋值为白色
+                    // 可以通过设置具体的值控制像素替换的范围
+                    if (rgb[0] >= 200 && rgb[1] >= 200 && rgb[2] >= 200) {
+
+                        image.setRGB(x, y, -1);
+                    } else {
+                        image.setRGB(x, y, pixel);
+                    }
+                }
+            }
+            try {
+                ImageIO.write(image, format, new File(path));
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+    }
+    
 
 }
